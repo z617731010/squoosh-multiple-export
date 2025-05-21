4b3535e0d4f4c64318048c433839f6a7b6d5ff7c
陈豪 <617731010@qq.com>
2025-05-21 15:31:26 +0800

    Delete codecs directory

--- codecs/qoi/dec/qoi_dec.d.ts
+++
@@ -1,7 +0,0 @@
-export interface QOIModule extends EmscriptenWasm.Module {
-  decode(data: BufferSource): ImageData | null;
-}
-
-declare var moduleFactory: EmscriptenWasm.ModuleFactory<QOIModule>;
-
-export default moduleFactory;
